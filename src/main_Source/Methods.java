d535fc9de29d9a4cf43ec50aa9fdea9ebba4e440
Pcase19 <paul.case@barstowschool.org>
2017-03-17 10:59:04 -0500

    Created images, slightly blurry, edited switchpoints, new normalized differences, prints out all iterations rather than 1

--- src/main_Source/Methods.java
+++ src/main_Source/Methods.java
@@ -15,24 +15,26 @@ import java.util.ArrayList;
 public class Methods {
 	int num_simpairs = 1000000;
 	public File importFile;
-    public static int max_its = 1000;
+    public static int max_its = 100;
     public static double res;
-    int[] firstxcords = new int[num_simpairs];
-    int[] firstycords = new int[num_simpairs];
-    int secxcord;
-    int secycord; 	
-    int diftemp;
-    int tempx;
-    int tempy;
-    int tempang;
-    public int radius = 2;
+//    int[] firstxcords = new int[num_simpairs];
+//    int[] firstycords = new int[num_simpairs];
+//    int secxcord;
+//    int secycord; 	
+//    int diftemp;
+//    int tempx;
+//    int tempy;
+//    int tempang;
+    public static int radius = 5;
     static float brain[][] = new float [1024][1024];
     ByteBuffer buffer = ByteBuffer.allocate(brain.length*Integer.BYTES);
     float angles[] = new float[num_simpairs];
     float difs[] = new float[num_simpairs];
     protected File defaultSaveFile = new File((System.getProperty("user.home") + System.getProperty("file.separator") + "Desktop\\BrainOutput.txt"));
         public static void main(String [] args)  {
-        	float[][] outbrain = null;
+        	float[][] smallestItBrain = null;
+        	float[][] biggestItBrain = null;
+        	float[][] outbrain =new float[1024][1024];
      	    float brain[][] =  importRawFile("C:\\Users\\Amesome Paul Gaming\\Downloads\\cerebrum-PET.raw");
      	    int it = 0;
      	    
@@ -46,12 +48,13 @@ public class Methods {
      	    while(it<max_its){
      	    	it++;
      	    	System.out.println("Runnning iteration " + it);
-     	    	//creating cordinate pairs
+     	    	//creating coordinate pairs
      	    	ArrayList<ArrayList<Float[]>> bigcords = new ArrayList<ArrayList<Float[]>> ();    
-         	    float[] difs = new float[1048576];
+         	    ArrayList<Float> difs = new ArrayList<Float>();
          	    for (int i=0; i<1024; i++){
          		   for (int j =0; j<1024; j++){
-         			   int[] seccordArray = findRandomPosition(i, j);
+         			   int[] seccordArray = findRandomPosition(i, j,radius);
+//         			   System.out.println("Second Cord {" + seccordArray[0] + ", " + seccordArray[1] + "}");
          			   Float[] firstCordsSet = new Float[2];
          			   firstCordsSet[0] = (float) i;
          			   firstCordsSet[1] = (float) j;
@@ -66,36 +69,67 @@ public class Methods {
          			   
          			   float seccord =  brain[seccordArray[0]][seccordArray[1]];
          			   float dif = findDifference(brain[i][j], seccord);
-         			   difs[i*1024+j] = dif;
+         			   difs.add(dif);
          			   
          		   }
          	   }
          	    float average = findAverageArray(difs);
          	    System.out.println("Average Diference:" + average);
-         	    outbrain = switchPoints(brain, average, bigcords);
-         	    
+         	    switchPoints(brain, average, bigcords);
+         	    if(it == 1){
+         	    	smallestItBrain = brain;
+         	    }
+         	   if(it == max_its){
+        	    	biggestItBrain = brain;
+        	    }
+         	   File outfile = new File("C:\\Users\\Amesome Paul Gaming\\Desktop\\outbrains\\outputbrain" + it+".txt");
+        	    try{
+        	    	if (!outfile.exists()){
+        	    		outfile.createNewFile();
+        	    	}
+        	    	StringBuilder b = new StringBuilder();
+       			for (int i = 0; i<1024; i++) {
+       				for (int j = 0; j<1024; j++){
+       					b.append("" +brain[i][j] + ", ");
+       				}
+       				b.append("\n");
+       			}
+       			String outputString = b.toString();
+       			Files.write(outfile.toPath(), outputString.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+        	    	}
+        	    	catch (FileNotFoundException e1) {
+        				e1.printStackTrace();
+        			} catch (IOException e) {
+        				e.printStackTrace();
+        			}
      	    }
-    
-     	    File outfile = new File("C:\\Users\\Amesome Paul Gaming\\Desktop\\outputbrain.txt");
-     	    try{
-     	    	if (!outfile.exists()){
-     	    		outfile.createNewFile();
+     	    for (int i = 0; i<1024; i++){
+     	    	for (int j = 0; j<1024; j++){
+     	    		outbrain[i][j] = smallestItBrain[i][j] * biggestItBrain[i][j];
+     	    		
      	    	}
-     	    	StringBuilder b = new StringBuilder();
-    			for (int i = 0; i<1024; i++) {
-    				for (int j = 0; j<1024; j++){
-    					b.append("" +outbrain[i][j] + ", ");
-    				}
-    				b.append("\n");
-    			}
-    			String outputString = b.toString();
-    			Files.write(outfile.toPath(), outputString.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
-     	    	}
-     	    	catch (FileNotFoundException e1) {
-     				e1.printStackTrace();
-     			} catch (IOException e) {
-     				e.printStackTrace();
-     			}
+     	    }
+     	   File outfile = new File("C:\\Users\\Amesome Paul Gaming\\Desktop\\outbrains\\outputbrainMulti.txt");
+   	    try{
+   	    	if (!outfile.exists()){
+   	    		outfile.createNewFile();
+   	    	}
+   	    	StringBuilder b = new StringBuilder();
+  			for (int i = 0; i<1024; i++) {
+  				for (int j = 0; j<1024; j++){
+  					b.append("" +outbrain[i][j] + ", ");
+  				}
+  				b.append("\n");
+  			}
+  			String outputString = b.toString();
+  			Files.write(outfile.toPath(), outputString.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+   	    	}
+   	    	catch (FileNotFoundException e1) {
+   				e1.printStackTrace();
+   			} catch (IOException e) {
+   				e.printStackTrace();
+   			}
+     	    
      	    
      	    }
         
@@ -105,7 +139,6 @@ public class Methods {
       int b2 = (value >>  8) & 0xff;
       int b3 = (value >> 16) & 0xff;
       int b4 = (value >> 24) & 0xff;
-
       return b1 << 24 | b2 << 16 | b3 << 8 | b4 << 0;
     }
    
@@ -179,128 +212,30 @@ catch(FileNotFoundException ex) {
 	 * @return Absolute Value of the difference of the points
 	 */
 	public static float findDifference(float SPoint, float EPoint){
-		return Math.abs(SPoint-EPoint);
+		return Math.abs(SPoint-EPoint)/(SPoint+EPoint);
 	}
 	/**
-	 * @param CurrentPositionRow the y value of the cordinate
-	 * @param CurrentPositionCol the x value of the cordinate
-	 * @return Returns a random Cordinate + pixels away from the cord you inputed. Is an array, [0] is the row, and [1] is the column
+	 * @param CurrentPositionRow the y value of the coordinate
+	 * @param CurrentPositionCol the x value of the coordinate
+	 * @return Returns a random Coordinate + pixels away from the cord you inputed. Is an array, [0] is the row, and [1] is the column
 	 */
-	public static int[] findRandomPosition(int CurrentPositionRow, int CurrentPositionCol){
-		
+	public static int[] findRandomPosition(int CurrentPositionRow, int CurrentPositionCol,int rad){
 		int Fincords[] = new int[2];
-		int randPosition = (int) (Math.random()*100 + 1);
-		if (randPosition == 1){
-			Fincords[0] = CurrentPositionRow-5;
-			Fincords[1] = CurrentPositionCol;
+		float randCol = (float) (Math.random() * rad*2 -rad);
+		float randRow = (float) (Math.random() * rad*2-rad);
+		int finX = (int) ((float) CurrentPositionRow + randRow);
+		int finY = (int) ((float) CurrentPositionCol + randCol);
+		if(finX >1023){
+			finX = (int) (CurrentPositionRow-randRow);
 		}
-		if (randPosition == 2){
-			Fincords[0] = CurrentPositionRow-5;
-			Fincords[1] = CurrentPositionCol +1;
+		if (finY >1023){
+			finY = (int) (CurrentPositionCol -randCol);
 		}
-		if (randPosition == 3){
-			Fincords[0] = CurrentPositionRow-4;
-			Fincords[1] = CurrentPositionCol +2;
-		}
-		if (randPosition == 4){
-			Fincords[0] = CurrentPositionRow-3;
-			Fincords[1] = CurrentPositionCol +3;
-		}
-		if (randPosition == 5){
-			Fincords[0] = CurrentPositionRow-2;
-			Fincords[1] = CurrentPositionCol +4;
-		}
-		if (randPosition == 6){
-			Fincords[0] = CurrentPositionRow-1;
-			Fincords[1] = CurrentPositionCol +5;
-		}
-		if (randPosition == 7){
-			Fincords[0] = CurrentPositionRow;
-			Fincords[1] = CurrentPositionCol +5;
-		}
-		if (randPosition == 8){
-			Fincords[0] = CurrentPositionRow+1;
-			Fincords[1] = CurrentPositionCol +5;
-		}
-		if (randPosition == 9){
-			Fincords[0] = CurrentPositionRow+2;
-			Fincords[1] = CurrentPositionCol+4;
-		}
-		if (randPosition == 10){
-			Fincords[0] = CurrentPositionRow+3;
-			Fincords[1] = CurrentPositionCol+3;
-		}
-		if (randPosition == 11){
-			Fincords[0] = CurrentPositionRow+4;
-			Fincords[1] = CurrentPositionCol+2;
-		}
-		if (randPosition == 12){
-			Fincords[0] = CurrentPositionRow+5;
-			Fincords[1] = CurrentPositionCol+1;
-		}
-		if (randPosition == 13){
-			Fincords[0] = CurrentPositionRow+5;
-			Fincords[1] = CurrentPositionCol;
-		}
-		if (randPosition == 14){
-			Fincords[0] = CurrentPositionRow+5;
-			Fincords[1] = CurrentPositionCol-1;
-		}
-		if (randPosition == 15){
-			Fincords[0] = CurrentPositionRow+4;
-			Fincords[1] = CurrentPositionCol-2;
-		}
-		if (randPosition == 16){
-			Fincords[0] = CurrentPositionRow+3;
-			Fincords[1] = CurrentPositionCol-3;
-		}
-		if (randPosition == 17){
-			Fincords[0] = CurrentPositionRow+2;
-			Fincords[1] = CurrentPositionCol-4;
-		}
-		if (randPosition == 18){
-			Fincords[0] = CurrentPositionRow+1;
-			Fincords[1] = CurrentPositionCol-5;
-		}
-		if (randPosition == 19){
-			Fincords[0] = CurrentPositionRow;
-			Fincords[1] = CurrentPositionCol-5;
-		}
-		if (randPosition == 20){
-			Fincords[0] = CurrentPositionRow-1;
-			Fincords[1] = CurrentPositionCol-5;
-		}
-		if (randPosition == 21){
-			Fincords[0] = CurrentPositionRow-2;
-			Fincords[1] = CurrentPositionCol-4;
-		}
-		if (randPosition == 22){
-			Fincords[0] = CurrentPositionRow-3;
-			Fincords[1] = CurrentPositionCol-3;
-		}
-		if (randPosition == 23){
-			Fincords[0] = CurrentPositionRow-4;
-			Fincords[1] = CurrentPositionCol-2;
-		}
-		if (randPosition == 24){
-			Fincords[0] = CurrentPositionRow-5;
-			Fincords[1] = CurrentPositionCol-1;
-		}
-		if (Fincords[0]<0){
-			Fincords[0] = Math.abs(Fincords[0]);
-		}
-		if (Fincords[1]<0){
-			Fincords[1] = Math.abs(Fincords[1]);
-		}
-		if (Fincords[0] >=1024){
-			Fincords[0] = CurrentPositionRow;
-		}
-		if (Fincords[1] >=1024){
-			Fincords[1] = CurrentPositionCol;
-		}
-				return Fincords;
-		}
-	
+		Fincords[0] = Math.abs(finX);
+		Fincords[1] = Math.abs(finY);
+		return Fincords;
+		
+	}
 	
 	public byte[] convertFloatToByte(float[][] brains){
 		byte[] fin = new byte[1024*1024];
@@ -314,60 +249,47 @@ catch(FileNotFoundException ex) {
 		}
 		return fin;
 	}
-	public void printRaw(byte [] brain, String address){
-		try {
-			DataOutputStream os = new DataOutputStream(new FileOutputStream(address));
-			os.write(brain);
-			os.close();
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-		
-	}
 	/**
 	 * 
-	 * @param array is the float array of diferences
-	 * @return the average difference between the cordinates
+	 * @param difs2 is the float array of differences
+	 * @return the average difference between the coordinates
 	 */
-	public static float findAverageArray(float[] array){
+	public static float findAverageArray(ArrayList<Float> difs2){
 		float i = 0;
 		float j = 0;
-		for (float x : array){
+		for (float x : difs2){
 			i = i+x;
 			j++;
 		}
 		float b = i/j;
+		
 		return b;
 		
 	}
 	
-	public static float[][] switchPoints(float[][] brains, float average, ArrayList<ArrayList<Float[]>> bigcords){
+	public static void switchPoints(float[][] brains, float average, ArrayList<ArrayList<Float[]>> bigcords){
+		float [][]brains1 = brains;
 		for (int i = 0; i<1024; i++){
 			for (int j = 0; j<1024; j++){
-				ArrayList<Float[]> cords = bigcords.get(i+j);
+				ArrayList<Float[]> cords = bigcords.get(i*1024+j);
 				cords.get(0);
 				Float[] secondCordsSet = cords.get(1);
-				float secondCordsSet1 = secondCordsSet[0];
-				int secondCordsSetsc1 = (int) secondCordsSet1;
-				int secondCordsSetsc2 = (int) secondCordsSet1;
-				float dif = findDifference(brains[i][j],brains[secondCordsSetsc1][secondCordsSetsc2] );
+				float secondCordsSetXCord = secondCordsSet[0];
+				float secondCordsSetYCord = secondCordsSet[1];
+				float dif = findDifference(brains[i][j],brains[(int) secondCordsSetXCord][(int) secondCordsSetYCord] );
 				
-				if (dif < average){
-					brain[secondCordsSetsc1][secondCordsSetsc2] = 0;
-				}
-				else if (dif> average){
-					
+				if (dif > average && brains[i][j] >44){
+					int[] newPos = findRandomPosition(i,j,radius);
+				//	float newVal  = (float) ( (((Math.random()*10 + 1)-5)/100)*brains1[i][j]);
+					float geometricAv1 = (float) Math.sqrt(brain[i][j]);
+					float geometricAv2 = (float) Math.sqrt(brains[(int) secondCordsSetXCord][(int) secondCordsSetYCord]);
+					float newVal  = (float) (geometricAv1*geometricAv2);
+					// float newVal  = (float) (brains[i][j] + (((Math.random()*10 + 1)-5)/100)*brains[i][j]);
+					brains1[(int) secondCordsSetXCord][(int) secondCordsSetYCord] = newVal;
+				//	brains[(int) secondCordsSetXCord][(int) secondCordsSetYCord] =brains1[(int) secondCordsSetXCord][(int) secondCordsSetYCord]*brains[(int) secondCordsSetXCord][(int) secondCordsSetYCord]
 				}
 			}
+			brains = brains1;
 		}
-		return brains;
-		
 	}
-
-	
 }
-
